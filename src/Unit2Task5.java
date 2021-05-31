097fb4950d5e51f6e279e75cd285be61f971e6dd
mosh <sergey.moshkin92@gmail.com>
2021-05-31 21:50:46 +0300

    Домашка готова

--- src/Unit2Task5.java
+++ src/Unit2Task5.java
@@ -1,5 +1,10 @@
 public class Unit2Task5 {
     public static void main(String[] args) {
-        float inch = 2.54f;
+        float x = 2.54f;
+        for (int i = 1; i<=20; i++){
+            System.out.println(i + " inch" + " = " + i*x + " SM ");
+        }
+
     }
+
 }
